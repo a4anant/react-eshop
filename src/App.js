d33f54fd099549adbd69f2da80aaa7e73a69cfb5
Anant Dhiman <developer.anant@gmail.com>
2024-06-25 22:46:45 +0530

    added router and link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from "./component/Checkout";
 
 function App() {
   return (
-    <div className="container-fluid">
-      <Header />
-      <Home />
+    <div className="container-fluid">      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Home />} />
+            <Route path="checkout" element={<Checkout />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/component/Checkout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/css/Checkout.css';
+
+function Checkout() {
+  return (
+    <div>Checkout content</div>
+  )
+}
+
+export default Checkout
